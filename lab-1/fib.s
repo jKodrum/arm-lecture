fb6137ea557343a2360651a1a42e8858125fcfac
jKodrum <jkodrum@gmail.com>
2015-10-03 22:12:14 +0800

    tail recursion
    1. implement fib by tail recursion
    
    2. modify coding style
    substitute '\t' with four spaces in fib.c
    indent with 4 spaces instead 2

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -1,46 +1,33 @@
-	.syntax unified
-	.arch armv7-a
-	.text
-	.align 2
-	.thumb
-	.thumb_func
+    .syntax unified
+    .arch armv7-a
+    .text
+    .align 2
+    .thumb
+    .thumb_func
 
-	.global fibonacci
-	.type fibonacci, function
+    .global fibonacci
+    .type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
+    @ ADD/MODIFY CODE BELOW
+    @ PROLOG
+    push {r3, r4, r5, lr}
 
-    sub r4, r0, #0          @ R4 = R0 - 0 (update flags)
-    
-    cmp r0, #0
-    ble .L3                 @ if(R0 <= 0) goto .L3 (which returns 0)
+    mov r3, r0              @ r3 = r0
+    mov r4, r1              @ r4 = r1
+    mov r5, r2              @ r5 = r2
+    cbz r3, .ret_r1         @ if (r3==0) return r1;
 
-    cmp r4, #1              @ Compare R4 wtih 1
-    beq .L4                 @ If R4 == 1 goto .L4 (which returns 1)
+    sub r0, r0, #1
+    mov r1, r2
+    add r2, r4, r5
+    bl fibonacci            @ fibonacci (r0-1, r2, r1+r2)
+    pop {r3, r4, r5, pc}        @EPILOG
 
-    sub r0, r4, #1          @ R0 = R4 - 1
-    bl fibonacci            @ Recursive call to fibonacci with R4 - 1 as parameter
+    @ END CODE MODIFICATION
+.ret_r1:
+    mov r0, r1
+    pop {r3, r4, r5, pc}        @ EPILOG
 
-    mov r5, r0              @ R5 = R0
-    sub r0, r4, #2          @ R0 = R4 - 2
-    bl fibonacci            @ Recursive call to fibonacci with R4 - 2 as parameter
-
-    add r0, r5, r0          @ R0 = R5 + R0 (update flags)
-
-    ;r0 = 5
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-	.size fibonacci, .-fibonacci
-	.end
+    .size fibonacci, .-fibonacci
+    .end
--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-extern int fibonacci(int x);
+extern int fibonacci(int x, int a, int b);
 
 int main(int argc, char **argv)
 {
-  int number=0;
-  int result=0;
+    int number=0;
+    int result=0;
 
-  scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
+    scanf("%d",&number);
+    result = fibonacci(number, 0, 1);   
+    printf("The fibonacci sequence at %d is: %d\n", number, result);
 }
 
